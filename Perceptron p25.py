ca9e09e2b79a3fea0d3c1e42754a32ebb31c5251
caldera12345 <register@davidhpbarron.com>
2019-03-24 13:50:31 -0400

    Update Perceptron p25.py

--- Perceptron p25.py
+++ Perceptron p25.py
@@ -52,12 +52,12 @@ class Perceptron(object):
         
         for _ in range(self.n_iter):
             errors = 0
-                for xi, target in zip(X, Y):
+            for xi, target in zip(X, Y):
                 update = self.eta*(target-self.predict(Xi))
                 self.w_[1:] += update *xi
                 self.w_[0] += update
                 errors += int(update != 0.0)
-                self.errors_.append(errors)
+            self.errors_.append(errors)
         return self
     
     def net_input(self, X):
